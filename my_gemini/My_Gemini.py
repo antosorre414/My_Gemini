be12b4396168f22e3ec94ab81447de47a6aa5c13
antosorre414 <antosorre13@gmail.com>
2026-02-13 16:45:17 +0100

    Update My_Gemini.py

--- my_gemini/My_Gemini.py
+++ my_gemini/My_Gemini.py
@@ -1066,7 +1066,7 @@ with st.sidebar:
 
        # ... dentro QUADRANTE 3 ...
         
-        memory_limit = st.number_input("⚡ Context Window (Msg)", min_value=0, max_value=100, value=5, help="Numero di messaggi passati da ricordare")
+        memory_limit = st.number_input("⚡ Context Window (Msg)", min_value=0, max_value=100, value=1, help="Numero di messaggi passati da ricordare")
         
         # --- NUOVO TOGGLE ---
         st.session_state.use_smart_rag = st.toggle("🧠 Compressione Lite", value=True, help="ON: Il modello Lite riassume la storia (Risparmio Token)\nOFF: Invia i messaggi grezzi (Massima Fedeltà)")
@@ -1826,6 +1826,7 @@ with st.sidebar:
         """, 
         unsafe_allow_html=True
     )
+
 
 
 
